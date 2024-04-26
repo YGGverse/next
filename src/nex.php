4764674730cb008fd9f889eac46940e203d9b9f3
yggverse <yggverse@project>
2024-04-27 00:49:22 +0300

    update comment

--- src/nex.php
+++ src/nex.php
@@ -6,7 +6,7 @@ require_once __DIR__ .
              DIRECTORY_SEPARATOR . 'vendor' .
              DIRECTORY_SEPARATOR . 'autoload.php';
 
-// Parse requested arguments
+// Parse startup arguments
 foreach ((array) $argv as $item)
 {
     if (preg_match('/^(?<key>[^=]+)=(?<value>.*)$/', $item, $argument))
